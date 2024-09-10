c3874b044895ae86c0bed37637d845a43f1f5ae7
hkushwah <hkushwah@iu.edu>
2024-09-09 21:54:32 -0400

    try 01

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Self_Portfolio/my-project/', // Add the base option
+  base: '/Self_Portfolio/', // This should match your GitHub repository name
 })
+
